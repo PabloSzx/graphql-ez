1f3086b8e2d082dd9f5afaba4753df45dcbdfcae
PabloSzx <pablosaez1995@gmail.com>
2021-06-28 22:48:07 -0400

    improve generic type names & fix recursive context reference

---
+++ .changeset/gorgeous-coats-laugh.md
@@ -0,0 +1,5 @@
+---
+'@graphql-ez/plugin-modules': patch
+---
+
+improve generic type names & fix recursive context reference
--- packages/plugin/dataloader/src/index.ts
+++ packages/plugin/dataloader/src/index.ts
@@ -3,17 +3,22 @@ import { useDataLoader } from '@envelop/dataloader';
 
 import type { EZPlugin, EZContext } from 'graphql-ez';
 
-export type DataLoaderFn<K, V, C = K> = (DataLoaderClass: typeof DataLoader, context: EZContext) => DataLoader<K, V, C>;
+export type DataLoaderFn<Key, Value, CacheKey = Key> = (
+  DataLoaderClass: typeof DataLoader,
+  context: EZContext
+) => DataLoader<Key, Value, CacheKey>;
 
-export type RegisteredDataLoader<Name extends string, Key, Value, Cache = Key> = {
+export type DataLoaderFactory<Key, Value, CacheKey> = () => DataLoader<Key, Value, CacheKey>;
+
+export type RegisteredDataLoader<Name extends string, Key, Value, CacheKey = Key> = {
   name: Name;
-  dataLoaderFactory: DataLoaderFn<Key, Value, Cache>;
+  dataLoaderFactory: DataLoaderFactory<Key, Value, CacheKey>;
 };
 
-export type RegisterDataLoader = <Name extends string, Key, Value, Cache = Key>(
+export type RegisterDataLoader = <Name extends string, Key, Value, CacheKey = Key>(
   name: Name,
-  dataLoaderFactory: DataLoaderFn<Key, Value, Cache>
-) => RegisteredDataLoader<Name, Key, Value, Cache>;
+  dataLoaderFactory: DataLoaderFn<Key, Value, CacheKey>
+) => RegisteredDataLoader<Name, Key, Value, CacheKey>;
 
 declare module 'graphql-ez/types/index' {
   interface BaseAppBuilder {
@@ -29,18 +34,18 @@ export const ezDataLoader = (): EZPlugin => {
   return {
     name: 'DataLoader',
     onRegister(ctx) {
-      function registerDataLoader<Name extends string, Key, Value, Cache = Key>(
+      function registerDataLoader<Name extends string, Key, Value, CacheKey = Key>(
         name: Name,
-        dataLoaderFactory: DataLoaderFn<Key, Value, Cache>
-      ): RegisteredDataLoader<Name, Key, Value, Cache> {
+        dataLoaderFactory: DataLoaderFn<Key, Value, CacheKey>
+      ): RegisteredDataLoader<Name, Key, Value, CacheKey> {
         ctx.options.envelop.plugins.push(
-          useDataLoader(name, context => {
+          useDataLoader<Key, Value, CacheKey, EZContext>(name, context => {
             return dataLoaderFactory(DataLoader, context);
           })
         );
         return {
           name,
-          dataLoaderFactory,
+          dataLoaderFactory: dataLoaderFactory as DataLoaderFactory<Key, Value, CacheKey>,
         };
       }
 
