4985b812c6afdaa09b1413109246165684bbc8f2
PabloSzx <pablosaez1995@gmail.com>
2021-08-28 22:15:37 -0400

    sveltekit integration progress

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,6 +10,16 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
+
+    adapter: adapter({
+      // default options are shown
+      out: 'build',
+      precompress: false,
+      env: {
+        host: 'HOST',
+        port: 'PORT',
+      },
+    }),
   },
 };
 
