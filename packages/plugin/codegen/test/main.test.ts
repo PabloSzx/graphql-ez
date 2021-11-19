9970248891541d7bb9298822861933e6198ae99b
PabloSzx <pablosaez1995@gmail.com>
2021-11-19 16:35:11 -0300

    update

--- packages/plugin/codegen/test/main.test.ts
+++ packages/plugin/codegen/test/main.test.ts
@@ -61,6 +61,7 @@ test('typescript resolvers', async () => {
     import type { EZContext } from 'graphql-ez';
     import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
     export type Maybe<T> = T | null;
+    export type InputMaybe<T> = Maybe<T>;
     export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
     export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
     export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
@@ -106,7 +107,7 @@ test('typescript resolvers', async () => {
       args: TArgs,
       context: TContext,
       info: GraphQLResolveInfo
-    ) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
+    ) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
 
     export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
       parent: TParent,
--- packages/plugin/dataloader/test/ez.generated.ts
+++ packages/plugin/dataloader/test/ez.generated.ts
@@ -1,6 +1,7 @@
 import type { GraphQLResolveInfo } from 'graphql';
 import type { EZContext } from './main.test';
 export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
@@ -47,7 +48,7 @@ export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
   args: TArgs,
   context: TContext,
   info: GraphQLResolveInfo
-) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
+) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
 
 export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
