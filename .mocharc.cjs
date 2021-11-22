3b2492b4ca2bfe454699f2bdb632cacdab982e46
PabloSzx <pablosaez1995@gmail.com>
2021-11-22 14:40:03 -0300

    improve & separate integration tests

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -1,11 +1,11 @@
-const { resolve } = require('path');
+const { join } = require('path');
 
 process.env.NODE_ENV = 'test';
 
 const config = {
   'enable-source-maps': true,
-  spec: [resolve(__dirname, '**/test-mocha/**/*.ts')],
-  require: [resolve(__dirname, './pre-mocha-test.mjs')],
+  spec: [join(process.cwd(), '**/test-mocha/**/*.ts')],
+  require: [join(__dirname, './pre-mocha-test.mjs')],
   extension: ['ts'],
   'node-option': ['require=bob-tsm', 'loader=bob-tsm'],
 };
---
+++ integration/gql-16-interop/test-mocha/fastify.test.ts
@@ -0,0 +1,40 @@
+import { CreateTestClient, GlobalTeardown } from '@graphql-ez/fastify-testing';
+import { ezSchema, gql } from '@graphql-ez/plugin-schema';
+import { expect } from 'chai';
+
+after(GlobalTeardown);
+
+test('Hello World', async () => {
+  const { assertedQuery } = await CreateTestClient({
+    ez: {
+      plugins: [
+        ezSchema({
+          schema: {
+            typeDefs: gql`
+              type Query {
+                hello: String!
+              }
+            `,
+            resolvers: {
+              Query: {
+                hello() {
+                  return 'Hello World!';
+                },
+              },
+            },
+          },
+        }),
+      ],
+    },
+  });
+
+  expect(
+    await assertedQuery(gql`
+      query {
+        hello
+      }
+    `)
+  ).to.deep.equals({
+    hello: 'Hello World!',
+  });
+});
