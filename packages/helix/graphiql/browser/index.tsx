cd456fd12a0fd4ffaa013dcefb7c2728e38b630e
Pablo Sáez <pablosaez1995@gmail.com>
2022-12-17 22:11:45 -0300

    pin graphiql deps

--- packages/helix/graphiql/browser/index.tsx
+++ packages/helix/graphiql/browser/index.tsx
@@ -70,7 +70,7 @@ const buildHybridMenuOptions = (hybridConfig: HybridSubscriptionTransportConfig,
 
 const getOperationWithFragments = (
   document: DocumentNode,
-  operationName: string
+  operationName: string | null | undefined
 ): {
   document: DocumentNode;
   isSubscriber: boolean;
@@ -198,13 +198,7 @@ export const init = async ({
       }, [menuOptions, hybridTransportIndex]);
 
       const onShare = () => {
-        const state = graphiqlRef.current?.state;
-
-        console.log({
-          a: state?.query,
-          b: state?.variables,
-          c: state?.operationName,
-        });
+        const state: Record<string, string> | undefined = graphiqlRef.current?.state;
 
         copyToClipboard(
           urlLoader.prepareGETUrl({
