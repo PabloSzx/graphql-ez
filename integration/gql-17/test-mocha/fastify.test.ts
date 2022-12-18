d1b780c42cea4ea16142ab7034d2eeeabc4e0a4a
Pablo Sáez <pablosaez1995@gmail.com>
2022-12-18 11:34:03 -0300

    update

--- integration/gql-17/test-mocha/fastify.test.ts
+++ integration/gql-17/test-mocha/fastify.test.ts
@@ -1,25 +1,27 @@
 import { CreateTestClient, GlobalTeardown } from '@graphql-ez/fastify-testing';
-import SchemaBuilder from '@pothos/core';
 import { expect } from 'chai';
 import { gql } from 'graphql-ez';
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 
 after(GlobalTeardown);
 
 test('Hello World', async () => {
-  const builder = new SchemaBuilder({});
-
-  builder.queryType({});
-
-  builder.queryField('hello', t =>
-    t.string({
-      resolve() {
-        return 'Hello World!';
-      },
-    })
-  );
-
   const { assertedQuery } = await CreateTestClient({
-    schema: builder.toSchema({}),
+    schema: new GraphQLSchema({
+      query: new GraphQLObjectType({
+        name: 'Query',
+        fields() {
+          return {
+            hello: {
+              type: GraphQLString,
+              resolve() {
+                return 'Hello World!';
+              },
+            },
+          };
+        },
+      }),
+    }),
   });
 
   expect(
