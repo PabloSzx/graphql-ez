f1210d77c0a59b3e038cce5ca1cbfead8fe0ef5b
Pablo Sáez <pablosaez1995@gmail.com>
2025-05-01 15:24:08 -0400

    Minor update everywhere

--- packages/plugin/upload/src/index.ts
+++ packages/plugin/upload/src/index.ts
@@ -1,6 +1,7 @@
 import { gql } from '@graphql-ez/utils/gql';
 import { getObjectValue } from '@graphql-ez/utils/object';
 import { LazyPromise } from '@graphql-ez/utils/promise';
+import type { RequestHandler } from 'express';
 import type { EZPlugin } from 'graphql-ez';
 import type { GraphQLUpload, graphqlUploadExpress, graphqlUploadKoa, processRequest, UploadOptions } from 'graphql-upload';
 
@@ -78,7 +79,9 @@ export const ezUpload = (options: GraphQLUploadConfig = true): EZPlugin => {
         async express({ integration: { router } }) {
           if (!ctx.options.path) throw Error("Path not specified and it's required for GraphQL Upload");
 
-          const graphqlUploadExpress = (await GraphQLUpload.express)(GraphQLUpload.options);
+          const graphqlUploadExpress: RequestHandler<any, any, any, any, any> = (await GraphQLUpload.express)(
+            GraphQLUpload.options
+          );
 
           router.post(ctx.options.path, graphqlUploadExpress, (_req, _res, next) => next());
         },
