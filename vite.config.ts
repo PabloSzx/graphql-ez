d0465ec4a0fe076eba5edfb30c1605c44c2cd0a9
Pablo Sáez <pablosaez1995@gmail.com>
2025-05-20 11:42:51 -0400

    clean

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    // testNamePattern: '**/test/**/*.test.ts',
     globalSetup: './setup-test.js',
     include: ['**/test/**/*.test.ts'],
     exclude: ['**/test-mocha/**/*.test.ts', '**/node_modules'],
